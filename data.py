e4ea9e186c8f5651c9d182d79b3c811efb370f05
sceaduwedubhar <sceaduwedubhar@gmail.com>
2017-02-22 12:55:22 +0800

    input data

--- data.py
+++ data.py
@@ -1,2 +1,8 @@
 import numpy as np
-import tensorflow
+import tensorflowimport tensorflowimport tensorflow as tf
+import gensim
+
+w2vmodel = gensim.models.Word2Vec.load_word2vec_format("GoogleNews-vectors-negative300.bin",binary=True)
+
+
+
